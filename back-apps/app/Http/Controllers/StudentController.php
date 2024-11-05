346a1f3e0b6e3c61a3ec9057b3661ff9f82f8726
muhamadnurraply <146099712+muhamadnurraply@users.noreply.github.com>
2024-11-05 22:01:04 +0700

    update

--- back-apps/app/Http/Controllers/StudentController.php
+++ back-apps/app/Http/Controllers/StudentController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student; // Tambahkan baris ini
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
@@ -12,6 +13,10 @@ class StudentController extends Controller
 
         // cara melihat data 
         $students = Student::all(); //pake eloquent
+
+        if ($students->isEmpty()) {
+            return response()->json(['message' => 'Tidak ada data mahasiswa ditemukan!'], 404);
+        }
         
         $response = [
             'message' => 'Berhasil Menampilkan Semua Data Mahasiswa',
@@ -21,19 +26,46 @@ class StudentController extends Controller
         return response()->json($response, 200);
     }
 
+    public function show($id) {
+        $student = Student::find($id);
+
+        if($student) {
+            if ($student) {
+            return response()->json($student, 200);
+            }
+    
+        return response()->json(['message' => 'Data Mahasiswa tidak ditemukan!!!'], 404);
+        
+        }
+     }
+
     public function create(){
         //
     }
    
     public function store(Request $request)
     {
-       $input = [
-        'nama' => $request->nama,
-        'nim' => $request->nim,
-        'email' => $request->email,
-        'jurusan' => $request->jurusan
-       ];
+        $validator = Validator::make($request->all(), [
+            'nama' => 'required|string|max:255',
+            'nim' => 'required|string|max:10',
+            'email' => 'required|email',
+            'jurusan' => 'required|string|max:100'
+        ], [
+            'nama.required' => 'Input gagal nama harus diisi dahulu.',
+            'nim.required' => 'Input gagal NIM harus diisi dahulu.',
+            'email.required' => 'Input gagal Email harus diisi dahulu.',
+            'email.email' => 'Format email tidak valid',
+            'jurusan.required' => 'Input gagal Jurusan harus diisi dahulu.',
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validasi gagal!!!',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $input = $validator->validated();
    
        $student = Student::create($input);
 
@@ -48,6 +80,7 @@ class StudentController extends Controller
   
     public function update(Request $request, string $id)
     {
+        // mencari student id
         $student = Student::find($id);
 
         if (!$student) {
@@ -55,10 +88,10 @@ class StudentController extends Controller
         }
 
         $input = [
-            'nama' => $request->nama,
-            'nim' => $request->nim,
-            'email' => $request->email,
-            'jurusan' => $request->jurusan,
+            'nama' => $request->nama ?? $student->nama,
+            'nim' => $request->nim ?? $student->nim,
+            'email' => $request->email ?? $student->email,
+            'jurusan' => $request->jurusan ?? $student->jurusan,
         ]; 
 
         $student->update($input);
@@ -84,4 +117,6 @@ class StudentController extends Controller
 
          return response()->json(['message' => 'Data Mahasiswa berhasil dihapuus'], 200);
      }
+
+     
 }
--- back-apps/routes/api.php
+++ back-apps/routes/api.php
@@ -9,6 +9,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 route::get('/student', [StudentController::class, 'index']);
+Route::get('/student/{id}', [StudentController::class, 'show']);
 route::post('/student', [StudentController::class, 'store']);
 Route::put('/student/{id}', [StudentController::class, 'update']);
 Route::delete('/student/{id}', [StudentController::class, 'destroy']);
