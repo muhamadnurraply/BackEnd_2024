484c33d408a57bf86d0ef16796346eaed34e744e
muhamadnurraply <146099712+muhamadnurraply@users.noreply.github.com>
2024-11-06 19:54:32 +0700

    update

--- back-apps/routes/api.php
+++ back-apps/routes/api.php
@@ -3,15 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AuthController;
+use App\Models\Student;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+route::get('/students', [StudentController::class, 'index'])->middleware('auth:sanctum');
 route::get('/student', [StudentController::class, 'index']);
 Route::get('/student/{id}', [StudentController::class, 'show']);
 route::post('/student', [StudentController::class, 'store']);
 Route::put('/student/{id}', [StudentController::class, 'update']);
 Route::delete('/student/{id}', [StudentController::class, 'destroy']);
+
+// route regis dan loginyaaa
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 // Route::apiResource('students', StudentController::class);
